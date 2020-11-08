aef8ea87bd5ea6a269ca9ffbbdae54017eabcad9
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2020-11-08 11:08:41 +0900

    Initial Commit

---
+++ 5*5gameAPp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  5*5gameAPp
+//
+//  Created by user on 2020/11/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
